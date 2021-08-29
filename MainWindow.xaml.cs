e144808c03d9868852bf7a8246d823009d28f838
Alexxx180 <alexx120702@gmail.com>
2021-08-29 22:04:09 +0300

    Release 1.0 - created StopWatch prototype

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,83 +1,138 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace StopWatch
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public static RoutedCommand PlayCmd = new RoutedCommand();
+        public static RoutedCommand PauseCmd = new RoutedCommand();
+        public static RoutedCommand StopCmd = new RoutedCommand();
+        Stopwatch watch = new Stopwatch();
         public MainWindow()
         {
             InitializeComponent();
-            Watch.Tick += Time;
+            DataContext = this;
+            Watch.Tick += TimeTick;
+            CommandBinding cb = new CommandBinding(PlayCmd, PlayExecuted, CanExecute);
+            CommandBindings.Add(cb);
+            CommandBinding cb1 = new CommandBinding(PauseCmd, PauseExecuted, CanExecute);
+            CommandBindings.Add(cb1);
+            CommandBinding cb2 = new CommandBinding(StopCmd, StopExecuted, CanExecute);
+            CommandBindings.Add(cb2);
+
+            PlayBtn.Command = PlayCmd;
+            PauseBtn.Command = PauseCmd;
+            StopBtn.Command = StopCmd;
+
+            KeyGesture kg = new KeyGesture(Key.F5);
+            InputBinding ib = new InputBinding(PlayCmd, kg);
+            InputBindings.Add(ib);
+            KeyGesture kg1 = new KeyGesture(Key.F6);
+            InputBinding ib1 = new InputBinding(PauseCmd, kg1);
+            InputBindings.Add(ib1);
+            KeyGesture kg2 = new KeyGesture(Key.F8);
+            InputBinding ib2 = new InputBinding(StopCmd, kg2);
+            InputBindings.Add(ib2);
         }
 
-        public byte[] time = { 0, 0, 0, 0, 0 };
-        private byte[] intervals = { 100, 60, 60, 24, 255 };
+        private void CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void PlayExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Play(sender, e);
+        }
+        private void PauseExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Pause(sender, e);
+        }
+        private void StopExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Stop(sender, e);
+        }
+
+        public byte[] Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private byte[] time = new byte[] { 0, 0, 0, 0, 0 };
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
 
         public DispatcherTimer Watch = new DispatcherTimer
         {
-            Interval = new TimeSpan(0, 0, 0, 0, 10),
+            Interval = TimeSpan.FromMilliseconds(1)
         };
-        private void Time(object sender, EventArgs e)
+        private void TimeTick(object sender, EventArgs e)
         {
-            for (byte i = 0; i < time.Length; i++)
-            {
-                if (time[i] + 1 < intervals[i])
-                {
-                    time[i]++;
-                    break;
-                }
-                time[i] = 0;
-            }
-            if (time[4] >= 255)
+            TimeSpan span = watch.Elapsed;
+            Time[0] = Convert.ToByte(span.Milliseconds / 100);
+            Time[1] = Convert.ToByte(span.Seconds);
+            Time[2] = Convert.ToByte(span.Minutes);
+            Time[3] = Convert.ToByte(span.Hours);
+            Time[4] = Convert.ToByte(span.Days);
+            if (Time[4] >= 255)
                 Stop();
+            OnPropertyChanged(nameof(Time));
         }
         private void Play(object sender, RoutedEventArgs e)
         {
+            watch.Start();
             Watch.Start();
-            Button play = sender as Button;
-            Hide(play);
-            Show(play.Tag as Button);
+            Change(PlayBtn, PauseBtn);
         }
         private void Pause(object sender, RoutedEventArgs e)
         {
+            if (watch.IsRunning)
+                watch.Stop();
             Watch.Stop();
-            Button pause = sender as Button;
-            Hide(pause);
-            Show(pause.Tag as Button);
+            Change(PauseBtn, PlayBtn);
         }
         private void Stop(object sender, RoutedEventArgs e)
         {
             Stop();
+            Change(PauseBtn, PlayBtn);
         }
         private void Stop()
         {
+            watch.Restart();
+            watch.Stop();
             Watch.Stop();
-            for (byte i = 0; i < time.Length; i++)
-                time[i] = 0;
+            for (byte i = 0; i < Time.Length; i++)
+                Time[i] = 0;
+            OnPropertyChanged(nameof(Time));
         }
-        private void Hide(Button btn)
+        private void Change(Button hide, Button show)
         {
-            btn.IsEnabled = false;
-            btn.Visibility = Visibility.Hidden;
-        }
-        private void Show(Button btn)
-        {
-            btn.IsEnabled = true;
-            btn.Visibility = Visibility.Visible;
+            hide.IsEnabled = false;
+            hide.Visibility = Visibility.Hidden;
+            show.IsEnabled = true;
+            show.Visibility = Visibility.Visible;
         }
     }
 }
---
+++ TimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StopWatch
+{
+    public class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            byte[] time = value as byte[];
+            return string.Format("{4:0}:{3:00}:{2:00}:{1:00}.{0:0}",
+              time[0], time[1], time[2], time[3], time[4]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
